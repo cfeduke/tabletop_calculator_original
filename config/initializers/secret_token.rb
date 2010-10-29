b6687d79ebf550599709cbfa39310f48737b3914
cfeduke <charles.feduke@gmail.com>
2010-10-29 00:26:50 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TabletopCalculator::Application.config.secret_token = '47f09a2d1cd58b78fe33c3802749a161c6b811c6cd788d376a02afee0e05c6d654a8090b3669b97c4013f0a7a5c3cba19d8121dffb59139556c6f28fcf84420a'
