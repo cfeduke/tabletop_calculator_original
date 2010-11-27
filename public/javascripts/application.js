de274f21f775f4dd66777e8c3187122d80716e3f
Charles Feduke <charles.feduke@gmail.com>
2010-11-27 18:38:04 -0500

    Bit better client side scripting.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,16 @@
 function remove_fields(link) {
-  $(link).prev("input[type=hidden]").val("1");
-  $(link).closest(".fields").hide();
+	$(link).prev("input[type=hidden]").val("1");
+	var fields = $(link).closest(".fields");
+	fields.hide();
+	fields.next(".add").hide();
 }
 	
 
 function add_fields(link, association, content) {
 	var new_id = new Date().getTime();
 	var regexp = new RegExp("new_" + association, "g");
-	$(link).parent().parent().before(content.replace(regexp, new_id));
+	var target = $(link).parent().prev(":visible");
+	if (target.length == 0)
+		target = $(link).parent();
+	target.append(content.replace(regexp, new_id));
 }
